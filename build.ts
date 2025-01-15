65678d3fad08a0f7874f9af5a0cd81703b80aa04
Kyle Gill <kylerobertgill@gmail.com>
2025-01-14 20:37:27 -0700

    chore: fix up ESM vs CJS

--- build.ts
+++ build.ts
@@ -1,8 +1,22 @@
+import type { BuildConfig } from "bun";
 import dts from "bun-plugin-dts";
 
-// Generates `dist/index.d.ts` at build time
-await Bun.build({
+const defaultBuildConfig: BuildConfig = {
   entrypoints: ["./src/index.ts"],
   outdir: "./dist",
-  plugins: [dts()],
-});
+};
+
+// Generates `dist/index.d.ts` at build time
+await Promise.all([
+  Bun.build({
+    ...defaultBuildConfig,
+    plugins: [dts()],
+    format: "esm",
+    naming: "[dir]/[name].js",
+  }),
+  Bun.build({
+    ...defaultBuildConfig,
+    format: "cjs",
+    naming: "[dir]/[name].cjs",
+  }),
+]);
---
+++ test.ts
@@ -0,0 +1,7 @@
+import { zuji } from "./dist/index.js";
+
+console.log(
+  zuji(1234.56, {
+    useGrouping: "always",
+  })
+);
